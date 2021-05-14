1287c4a580f58d55ee28c35cd93858d4047eb566
D.A.N_3002 <dinhanh300229@gmail.com>
2021-05-14 23:31:15 +0700

    [ADD] Add express

--- app/index.js
+++ app/index.js
@@ -1,9 +1,10 @@
-import functions from 'firebase-functions';
+import * as functions from 'firebase-functions';
+import express from 'express';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.get('/test', (req, res) => {
+	res.send('lkasdj');
+});
+
+exports.app = functions.https.onRequest(app);
