3363abb930bbb72c53080d4ef82c2f70ef222af7
D.A.N_3002 <dinhanh300229@gmail.com>
2021-05-15 16:48:48 +0700

    [ADD] Add env

--- app/index.js
+++ app/index.js
@@ -1,10 +1,13 @@
 import * as functions from 'firebase-functions';
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
 app.get('/test', (req, res) => {
-	res.send('lkasdj');
+	res.send(process.env.DB_HOST);
 });
 
 exports.app = functions.https.onRequest(app);
